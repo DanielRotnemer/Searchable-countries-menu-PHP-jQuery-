2b379741277a4f96d2244eac07eb412ca8b5b9ed
DanielRotnemer <91946386+DanielRotnemer@users.noreply.github.com>
2021-10-18 15:52:02 +0300

    Add files via upload

--- countries.php
+++ countries.php
@@ -3,7 +3,7 @@
     require_once('utilities/utilities.php');
 
     $myCountry = 'United States';
-    $myCountryFlagSrc = 'http://localhost/utilities/flags/blank.gif';
+    $myCountryFlagSrc = 'flags/blank.gif';
     $myCountryFlagClass = 'class="flag flag-us"';
 
     $countries = DigitalCountryCodesArray();
@@ -31,10 +31,10 @@
                 $phoneCode = $countryData['d_code'];
                 $image = '';
                 if ($code == 'gf' || $code == 'gp' || $code == 'io' || $code == 'pm' || $code == 're' || $code == 'xk') {
-                    $image = 'src="http://localhost/utilities/alt_flags'.$code.'.jpg"';
+                    $image = 'src="alt_flags/'.$code.'.jpg"';
                 }
                 else {
-                    $image = 'src="http://localhost/utilities/flags/blank.gif" class="flag flag-'.$code.'"';
+                    $image = 'src="flags/blank.gif" class="flag flag-'.$code.'"';
                 }
 
                 $color = $countryData['name'] == $myCountry ? 'style="color: #135bb8;"' : '';
@@ -66,11 +66,11 @@
         <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="stylesheet" href="countries.css?c=<?php echo time(); ?>"/>
-        <link rel="stylesheet" href="http://localhost/CountriesMenu/flags/flags.css"/>
-        <link rel="stylesheet" href="http://localhost/CountriesMenu/flags/flags.min.css"/>
+        <link rel="stylesheet" href="flags/flags.css"/>
+        <link rel="stylesheet" href="flags/flags.min.css"/>
         <script src="countries.js?c=<?php echo time(); ?>"></script>
-        <link rel="stylesheet" href="http://localhost/CountriesMenu/fonts/proximanova/stylesheet.css"/>
-        <link rel="stylesheet" href="http://localhost/CountriesMenu/fonts/proximanova_bold/stylesheet.css"/>
+        <link rel="stylesheet" href="fonts/proximanova/stylesheet.css"/>
+        <link rel="stylesheet" href="fonts/proximanova_bold/stylesheet.css"/>
     </head>
     <body>
         <div class="body-wrapper">
